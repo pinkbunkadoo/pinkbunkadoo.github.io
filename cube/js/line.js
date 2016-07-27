224ee4a7a0cfb4984260b4e35e58e712cb29fd71
David <dcroeser@gmail.com>
2016-07-27 14:58:46 +0100

    refresh

--- cube/js/line.js
+++
@@ -1,8 +0,0 @@
-
-function Line(a, b, color) {
-  // console.log('Line', a, b, color);
-  this.a = a;
-  this.b = b;
-  this.color = color;
-}
-
--- cube/js/view.js
+++
@@ -1,8 +0,0 @@
-
-function View(x, y, width, height, camera) {
-  this.x = (x === undefined ? 0 : x);
-  this.y = (y === undefined ? 0 : y);
-  this.width = (width === undefined ? 0 : width);
-  this.height = (height === undefined ? 0 : height);
-  this.camera = camera;
-}
